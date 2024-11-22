27cbbd098d1784f6fff2e642a8cad517090eb7a6
ebcode2021 <ebcode2021@gmail.com>
2024-11-22 14:27:37 +0900

    init commit

---
+++ src/main/java/com/what_plus/what_plus/WhatPlusApplication.java
@@ -0,0 +1,13 @@
+package com.what_plus.what_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/what_plus/what_plus/WhatPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.what_plus.what_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
